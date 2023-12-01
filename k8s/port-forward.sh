94df15aaeaa66181d1a88be445becee60ab3c0fd
Amit Personal <amitbiradar648@gmail.com>
2023-12-01 09:56:16 +0530

    Database

--- k8s/port-forward.sh
+++ k8s/port-forward.sh
@@ -3,3 +3,5 @@
 nohup kubectl port-forward services/$(kubectl get services | grep grafana | awk '{print$1}') 3000:80 
 
 nohup kubectl port-forward services/$(kubectl get services | grep prometheus-operated | awk '{print$1}') 9090:9090
+
+helm install mysqldb prometheus-community/prometheus-mysql-exporter --set user="exporter" --set pass="password"
